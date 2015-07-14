c9ceb3b50db87625689e1f9b5e0d457a7fc39fbc
Hassan Zamani <hsn.zamani@gmail.com>
2015-07-15 00:32:19 +0430

    add source map and explicit options

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var sysPath = require('path');
 
 function LiveScriptCompiler(config) {
   this.config = config;
+  plugins = config.plugins && config.plugins.livescript;
+  this.map = !!config.sourceMaps ? 'linked' : 'none';
+  this.bare = plugins && plugins.bare;
+  if (this.bare == undefined) this.bare = true;
+  this.const = plugins && plugins.const;
+  if (this.const == undefined) this.const = false;
 }
 
 LiveScriptCompiler.prototype.brunchPlugin = true;
@@ -10,18 +16,26 @@ LiveScriptCompiler.prototype.type = 'javascript';
 LiveScriptCompiler.prototype.extension = 'ls';
 
 LiveScriptCompiler.prototype.compile = function(data, path, callback) {
-  var err, error, result;
+  var compiled;
   try {
-    return result = livescript.compile(data, {
-      bare: true,
+    compiled = livescript.compile(data, {
+      filename: path,
+      map: this.map,
+      bare: this.bare,
+      const: this.const,
       header: false
     });
-  } catch (_error) {
-    err = _error;
-    return error = err;
-  } finally {
-    callback(error, result);
+  } catch (error) {
+    callback(error.toString());
+    return;
   }
+  if (this.map === 'linked') {
+    var code = compiled.code.replace("//# sourceMappingURL=undefined.map\n", "")
+    callback(null, {data: code, map: compiled.map.toString()});
+  } else {
+    callback(null, compiled);
+  }
+  return;
 };
 
 module.exports = LiveScriptCompiler;
